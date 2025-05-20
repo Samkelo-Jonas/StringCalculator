52eeadcf121441fa2800276725dfa5fed430b3e5
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-20 08:16:28 +0200

    Implemented a new line delimiter returns a sum test

--- StringCalculator.Tests/TestStringCalculatorKataFour.cs
+++ StringCalculator.Tests/TestStringCalculatorKataFour.cs
@@ -65,5 +65,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenNewLineDelimiter_ReturnsSum()
+        {
+            // Arrange
+            var numbers = "1\n2,3";
+            var expected = "6";
+            var stringCalculator = new StringCalculatorKataFour();
+            
+            // Act
+            var result = stringCalculator.Add(numbers);
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
