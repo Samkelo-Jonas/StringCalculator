95e643e9ec9bd18951507044ba52f4e3524278ac
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-21 09:28:02 +0200

    Implemented a negative number throw exception test

--- StringCalculator/StringCalculatorKataFive.cs
+++ StringCalculator/StringCalculatorKataFive.cs
@@ -19,16 +19,24 @@ namespace StringCalculator
             var sum = 0;
             char[] delimiters = { ',', '\n', '/', ';' };
             var numbersArray = numbers.Split(delimiters);
+            var negativeNumbers = new List<int>();
 
             foreach (var number in numbersArray)
             {
                 if (!string.IsNullOrEmpty(number))
                 {
+                    if (int.Parse(number) < 0)
+                    {
+                        negativeNumbers.Add(int.Parse(number));
+                    }
                     sum += int.Parse(number);
                 }
 
             }
-
+            if (negativeNumbers.Any())
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
+            }
             return sum.ToString();
         }
     }
