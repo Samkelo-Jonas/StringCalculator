a8387a937501a6400c1805208fc1acfc3e3d2844
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-22 07:56:45 +0200

    Implemented empty string test

---
+++ StringCalculator/StringCalculatorKataSix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculatorKataSix
+    {
+        public string Add(string numbers)
+        {
+            var sum = 0;
+
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return "0";
+            }
+
+            return sum.ToString();
+
+        }
+    }
+}
