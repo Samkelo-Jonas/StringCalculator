73faa0c024d7552991825575bbf5edab1d6648d0
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-23 11:34:01 +0200

    Implemented a new line return sum test

--- StringCalculator.Tests/TestStringCalculatorKataEight.cs
+++ StringCalculator.Tests/TestStringCalculatorKataEight.cs
@@ -64,5 +64,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenNewLine_ShouldReturnTheSum()
+        {
+            // Arrange
+            var numbers = "1\n2, 3";
+            var expected = "6";
+            var stringCalculator = new StringCalculatorKataEight();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
