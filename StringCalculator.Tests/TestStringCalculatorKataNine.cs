6a46dcfc153c28f3580d969c38cdbd19317d4f0a
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-23 15:11:14 +0200

    Implemented long custom delimiter return sum test

--- StringCalculator.Tests/TestStringCalculatorKataNine.cs
+++ StringCalculator.Tests/TestStringCalculatorKataNine.cs
@@ -140,5 +140,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenLongCustomDelimiter_ShouldReturnSum()
+        {
+            // Arrange
+            var numbers = "//[|||]\n1|||2|||3";
+            var expected = "6";
+            var stringCalculator = new StringCalculatorKataNine();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
