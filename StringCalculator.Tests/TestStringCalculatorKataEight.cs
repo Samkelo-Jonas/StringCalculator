3752507b177ec6d3e9d3535075d9bf6248e22986
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-23 10:54:31 +0200

    Added empty string return zero test

---
+++ StringCalculator.Tests/TestStringCalculatorKataEight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator.Tests
+{
+    public class TestStringCalculatorKataEight
+    {
+        [Fact]
+        public void Add_GivenEmptyString_ShouldReturn0()
+        {
+            // Arrange
+            var numbers = "";
+            var expected = "0";
+            var stringCalculator = new StringCalculatorKataEight();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorKataEight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class StringCalculatorKataEight
+    {
+        public string Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return "0";
+            }
+            return numbers;
+        }
+    }
+}
