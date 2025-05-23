196079aef7fff1fab98cba1ef2ca5e6708ed6313
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-23 08:39:51 +0200

    Implemented a new line between numbers returns a sum test

--- StringCalculator.Tests/TestStringCalculatorKataSeven.cs
+++ StringCalculator.Tests/TestStringCalculatorKataSeven.cs
@@ -65,5 +65,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenNewLine_ShouldReturnSum()
+        {
+            // Arrange
+            var numbers = "1\n2, 3";
+            var expected = "6";
+            var stringCalculator = new StringCalculatorKataSeven();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
