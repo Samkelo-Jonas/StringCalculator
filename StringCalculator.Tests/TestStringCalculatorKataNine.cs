fc7226946b067b7eadfbb3c3a06b5269655886ca
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-23 14:22:48 +0200

    Implemented multiple numbers return sum test

--- StringCalculator.Tests/TestStringCalculatorKataNine.cs
+++ StringCalculator.Tests/TestStringCalculatorKataNine.cs
@@ -52,5 +52,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheSum()
+        {
+            // Arrange
+            var numbers = "1, 2, 3";
+            var expected = "6";
+            var stringCalculator = new StringCalculatorKataNine();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
