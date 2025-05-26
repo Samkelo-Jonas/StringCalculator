7352022f78ad4c0916a80f0799b9e5a53c3ecd04
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-26 08:16:02 +0200

    Implemented an empty string test

---
+++ StringCalculator.Tests/TestStringCalculatorKataTen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator.Tests
+{
+    public class TestStringCalculatorKataTen
+    {
+        [Fact]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            // Arrange
+            var numbers = "";
+            var expected = "0";
+            var stringCalculator = new StringCalculatorKataTen();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
