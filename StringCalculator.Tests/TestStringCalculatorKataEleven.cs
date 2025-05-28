93aa1fe63bb8468f0c7565876694659dfc6d07de
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-28 08:29:57 +0200

    Implemented an empty string return zero test

---
+++ StringCalculator.Tests/TestStringCalculatorKataEleven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator.Tests
+{
+    public class TestStringCalculatorKataEleven
+    {
+        [Fact]
+        public void Add_GivenAnEmptyString_ShouldReturn0()
+        {
+            // Arrange
+            var numbers = "";
+            var expected = "0";
+            var stringCalculator = new StringCalculatorKataEleven();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
