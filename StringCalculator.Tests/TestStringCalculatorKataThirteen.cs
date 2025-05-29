5b373c4d4ce7406293bddb031e137bbc584578b7
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-29 10:50:14 +0200

    Implemented two numbers test

--- StringCalculator.Tests/TestStringCalculatorKataThirteen.cs
+++ StringCalculator.Tests/TestStringCalculatorKataThirteen.cs
@@ -37,5 +37,20 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenTwoNumbers_ShouldReturnTheirSum()
+        {
+            // Arrange
+            var numbers = "1, 2";
+            var expected = "3";
+            var stringCalculator = new StringCalculatorKataThirteen();
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
