c90a44f00794cd95a75f4682258cf9405e2fee10
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-29 14:21:26 +0200

    Implemented an empty string test

---
+++ StringCalculator.Tests/TestStringCalculatorKataFourteen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    public class TestStringCalculatorKataFourteen : IDisposable
+    {
+        private StringCalculatorKataFourteen stringCalculator;
+        public TestStringCalculatorKataFourteen()
+        {
+            stringCalculator = new StringCalculatorKataFourteen();
+        }
+
+        [Fact]
+        public void Add_GivenAnEmptyString_ShouldReturn0()
+        {
+            // Arrange
+            var numbers = "";
+            var expected = "0";
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
