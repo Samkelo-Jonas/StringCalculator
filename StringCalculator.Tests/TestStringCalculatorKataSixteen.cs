54eac350809983b682d8fee3efb75066b4b3e004
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-30 10:00:08 +0200

    Implemented a custom delimiter returns sum test

--- StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
+++ StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
@@ -83,5 +83,19 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenSingleCustomDelimiter_ShouldReturnsSum()
+        {
+            // Arrange
+            var numbers = "//;\n1;2";
+            var expected = "3";
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
