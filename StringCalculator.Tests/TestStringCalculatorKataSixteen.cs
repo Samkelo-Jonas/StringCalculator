f73b2975c7b8db40fd595c9ec5afac15354f129c
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-30 10:05:23 +0200

    Implemented a negative number throws exception test

--- StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
+++ StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
@@ -97,5 +97,18 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenOneNegativeNumber_ShouldThrowException()
+        {
+            // Arrange
+            var numbers = "-1, 2";
+
+            // Act
+            var exception = Assert.Throws <ArgumentException> (() => stringCalculator.Add(numbers));
+
+            // Assert
+            Assert.Equal("Negatives not allowed: -1", exception.Message);
+        }
     }
 }
