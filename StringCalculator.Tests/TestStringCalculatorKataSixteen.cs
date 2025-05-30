36bbb53e9d3472b4b33b060d6dd70eae3038abd4
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-05-30 10:13:34 +0200

    Implemented long multiple delimiters return sum test

--- StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
+++ StringCalculator.Tests/TestStringCalculatorKataSixteen.cs
@@ -151,5 +151,19 @@ namespace StringCalculator.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Add_GivenMultipleLongCustomDelimiters_ShouldReturnsSum()
+        {
+            // Arrange
+            var numbers = "//[|||][***]\n1|||2***3";
+            var expected = "6";
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
