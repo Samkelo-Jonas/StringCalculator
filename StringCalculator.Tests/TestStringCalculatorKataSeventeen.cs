246f0e9408d0688add6b30b0f7f80c8c62796cd3
Samkelo-Jonas <samkeloj@betsoftware.com>
2025-06-02 08:09:03 +0200

    Implemented an empty string test

---
+++ StringCalculator.Tests/TestStringCalculatorKataSeventeen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    public class TestStringCalculatorKataSeventeen
+    {
+        private StringCalculatorKataSeventeen stringCalculator;
+        public TestStringCalculatorKataSeventeen()
+        {
+            stringCalculator = new StringCalculatorKataSeventeen();
+        }
+
+        [Fact]
+        public void Add_GivenEmptyString_ShouldReturn0()
+        {
+            // Arrange
+            var numbers = "";
+            var expected = "0";
+
+            // Act
+            var result = stringCalculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorKataSeventeen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculatorKataSeventeen
+    {
+        public string Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return "0";
+            }
+
+            return numbers;
+        }
+    }
+}
